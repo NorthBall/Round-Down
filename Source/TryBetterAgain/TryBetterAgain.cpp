c578f82ae66c12814c9b44cd541e4db9ee235016
NorthBall <Korsun.andrew@yandex.ru>
2017-09-14 12:19:55 +0300

    first

---
+++ Source/TryBetterAgain/TryBetterAgain.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "TryBetterAgain.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TryBetterAgain, "TryBetterAgain" );
+
+DEFINE_LOG_CATEGORY(LogTryBetterAgain)
+ 
