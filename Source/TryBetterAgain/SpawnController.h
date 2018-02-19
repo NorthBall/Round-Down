64fe1b3baafd46e969a8d5b346f413b2a15fbec1
korsun.andrew <korsun.andrew@yandex.ru>
2018-02-19 21:25:24 +0300

    polet normal'nii

--- Source/TryBetterAgain/SpawnController.h
+++ Source/TryBetterAgain/SpawnController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Runtime/Engine/Classes/Engine/DataTable.h"
 #include "SpawnController.generated.h"
 
 /**
@@ -15,7 +16,15 @@ class TRYBETTERAGAIN_API ASpawnController : public AAIController
 	GENERATED_BODY()
 public:
 		TDoubleLinkedList <class AAI*> Bots;
-		void SpawnBots() {};
+		UFUNCTION(BlueprintCallable)
+		void SpawnBots () ;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UDataTable * BotsTable;
+		//inputdata
+		TLinkedList<enum class BotType> TypeToSpawn;
+		TLinkedList<int32> LvlToSpawn;
+		//suppoting
+		TArray<FName> RowsFName;
 	
 	
 };
